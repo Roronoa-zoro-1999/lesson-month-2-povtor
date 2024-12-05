af42b5720b6c91f93d95501934847d2026cd52a9
Arnel Tilekov <arneltilekov4@gmail.com>
2024-12-05 23:35:01 +0800

    add lesson 1

---
+++ lesson_1/app.js
@@ -0,0 +1,13 @@
+// setTimeout(() => {
+//     document.body.style.backgroundColor = "green"
+// }, 3000)
+//
+// document.body.style.fontSize = "108px"
+// let c = 100;
+// setInterval(() => {
+//     document.body.innerHTML = c--;
+// }, 1000);
+
+alert("Hello Group 45 ALERT");
+document.body.innerHTML = "Helle Group 45 HTML";
+console.log("Helle Group 45 DEVTOOLS");
